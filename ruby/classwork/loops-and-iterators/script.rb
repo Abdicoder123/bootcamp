6f5213bdddca53f579423b252c7116d8422bf981
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2024-10-29 12:32:10 -0500

    Pushing changes

---
+++ ruby/classwork/loops-and-iterators/script.rb
@@ -0,0 +1,20 @@
+#The while loop
+i = 1
+while i < 21
+    if i % 2 == 0
+        puts "This is an even number"
+    else
+        puts "This is an odd number"
+    end
+    i += 1
+end
+
+#The for loop
+
+for x in 1..20
+    if x % 2 == 0
+        puts "This is an even number"
+    else
+        puts "This is an odd number"
+    end 
+end
