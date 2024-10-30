987221c04ce6efe6d356b0802bb417d56507065b
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2024-10-30 08:29:01 -0500

    last changes

---
+++ ruby/homework/word-analyzer/script.rb
@@ -0,0 +1,25 @@
+# Sample list of words
+words = ["level", "elephant", "ruby", "book", "hello"]
+for i in words
+    puts "Word: #{i}"
+    reverse_word = i.reverse.downcase
+    if i == reverse_word
+        puts "- Palindrome: Yes"
+    elsif i != reverse_word
+        puts "- Palindrome: No"
+    elsif i.include? "e"
+        puts "- Contains 'e': Yes"
+    elsif !i.include? "e"
+        puts "- Contains 'e': No"
+    end
+    
+    length_of_word = i.length
+    puts "- Character count: #{length_of_word}"
+    if i.include? "ruby"
+        replacement = i.gsub!("ruby", "Python")
+        puts "- After Replacement: #{replacement}"
+    else
+        replacement = i
+        puts "- After Replacement: #{replacement}"
+    end
+end
