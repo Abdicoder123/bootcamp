227ebaecb0809287c6af3187eebb84096ff98aa8
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2024-11-10 21:28:44 -0600

    This is the weekend assignment

--- ruby/homework/blog/db/schema.rb
+++ ruby/homework/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_07_192954) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_10_230940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_07_192954) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "priority_level", default: 0, null: false
   end
 
   create_table "comments", force: :cascade do |t|
